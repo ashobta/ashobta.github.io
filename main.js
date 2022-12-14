cce1b140f7ccd1eb341efbf10fc1fc313be0d23e
ashobta <ashobta.azry@gmail.com>
2022-12-15 06:47:20 +0700

    Change style

--- main.js
+++ main.js
@@ -13,14 +13,41 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $('#courses').click(function() {
-      $('#courlist').slideToggle();
-      $('#courses_span').toggle();
-    });
+  $('#ykgwlist2').click(function() {
+    $('#ykgwlist2_p').slideToggle();
+    $('#ykgwlist2_span').toggle();
+  });
 });
+
+$(document).ready(function(){
+  $('#ykgwlist3').click(function() {
+    $('#ykgwlist3_p').slideToggle();
+    $('#ykgwlist3_span').toggle();
+  });
+});
+
+$(document).ready(function(){
+  $('#ykgwlist4').click(function() {
+    $('#ykgwlist4_p').slideToggle();
+    $('#ykgwlist4_span').toggle();
+  });
+});
+
+// $(document).ready(function(){
+//     $('#courses').click(function() {
+//       $('#courlist').slideToggle();
+//       $('#courses_span').toggle();
+//     });
+// });
 
 $(document).ready(function(){
     $('#telkom').click(function() {
       $('#tellist').toggle("slide");
     });
+});
+
+$(document).ready(function(){
+  $('#cmps').click(function() {
+    $('#cmpslist').toggle("slide");
+  });
 });
