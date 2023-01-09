87af9ecac22410a601e17805cde21d1547721435
ashobta <63583064+ashobta@users.noreply.github.com>
2023-01-09 15:11:13 +0700

    feat: navbar transparency and smooth sliding

--- main.js
+++ main.js
@@ -1,53 +1,19 @@
-$(document).ready(function(){
-    $('#yokogawa').click(function() {
-      $('#ykgwlist').slideToggle();
-      $('#ykgwplus').toggle();
+
+window.addEventListener("scroll", function() {
+  var navbar = document.querySelector(".navbar");
+  if (window.scrollY > 10) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+document.querySelectorAll('.navbar a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: "smooth", block: "start"
     });
-});
-
-$(document).ready(function(){
-  $('#ykgwlist1').click(function() {
-    $('#ykgwlist1_p').slideToggle();
-    $('#ykgwlist1_span').toggle();
-  });
-});
-
-$(document).ready(function(){
-  $('#ykgwlist2').click(function() {
-    $('#ykgwlist2_p').slideToggle();
-    $('#ykgwlist2_span').toggle();
-  });
-});
-
-$(document).ready(function(){
-  $('#ykgwlist3').click(function() {
-    $('#ykgwlist3_p').slideToggle();
-    $('#ykgwlist3_span').toggle();
-  });
-});
-
-$(document).ready(function(){
-  $('#ykgwlist4').click(function() {
-    $('#ykgwlist4_p').slideToggle();
-    $('#ykgwlist4_span').toggle();
-  });
-});
-
-// $(document).ready(function(){
-//     $('#courses').click(function() {
-//       $('#courlist').slideToggle();
-//       $('#courses_span').toggle();
-//     });
-// });
-
-$(document).ready(function(){
-    $('#telkom').click(function() {
-      $('#tellist').toggle("slide");
-    });
-});
-
-$(document).ready(function(){
-  $('#cmps').click(function() {
-    $('#cmpslist').toggle("slide");
   });
 });
