167f82ed7ff1e7a9f226b103784f925c558e7100
ashobta <ashobta.azry@gmail.com>
2024-05-11 17:52:25 +0700

    feat: add header and image background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import InputForm from './components/inputform';
+import Navbar1 from './components/navbar';
+import HeaderPage from './components/header';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ashobta's Web V2</h1>
-      <h2>Coming Soon!</h2>
-    </div>
+    <>
+    {/* <Navbar1 /> */}
+    <HeaderPage />
+    </>
+      
+
   );
 }
 
+
 export default App;
+
